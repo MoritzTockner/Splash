bf20f26d390a0f80a52188d60eeab234eacd3bf9
MoritzTockner <moritz.tockner@a1.net>
2018-04-16 11:23:08 +0200

    Third commit
    without gitattributes

---
+++ SplashProject/assets/Scripts/EnemyScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+	HUDScript hud;
+
+	public Sprite[] mSprites;
+	private float switchTime = 0.005f;
+	private int counter;
+	private SpriteRenderer spriteRenderer;
+
+	void Start() {
+		counter = 0;
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		StartCoroutine ("SwitchSprite");
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.name == "Fish") {
+			hud = GameObject.Find ("Main Camera").GetComponent<HUDScript> ();
+			hud.DecreaseLife ();
+		}
+	}
+
+	private IEnumerator SwitchSprite() {
+		spriteRenderer.sprite = mSprites [counter];
+		if (counter < mSprites.Length - 1) {
+			counter++;
+		} else {
+			counter = 0;
+		}
+
+		yield return new WaitForSeconds (switchTime);
+		StartCoroutine ("SwitchSprite");
+	}
+}
---
+++ SplashProject/assets/Scripts/SpawnBackgroundScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBackgroundScript : MonoBehaviour {
+
+	public GameObject obj;
+	private GameObject fishObj;
+	private float relativeFishPos;
+	private Vector3 StartPos;
+	public float TraveledDistance = 0f;
+	private bool Spawned = false;
+	public float SpawnDistance = 1f;
+
+	void Start () {
+		StartPos = transform.position;
+		fishObj = GameObject.Find ("Fish");
+		relativeFishPos = transform.position.x - fishObj.transform.position.x;
+	}
+
+	// Use this for initialization
+	void FixedUpdate() {
+		TraveledDistance = (StartPos - transform.position).magnitude;
+		transform.position += Vector3.right * (fishObj.transform.position.x + relativeFishPos - transform.position.x);
+
+		if (TraveledDistance % SpawnDistance <= SpawnDistance / 2 && Spawned == false) {
+			Spawn ();
+			Spawned = true;
+		}
+		if (TraveledDistance % SpawnDistance >= SpawnDistance / 2) {
+			Spawned = false;
+		}
+	}
+
+	void Spawn() {
+		Instantiate (obj, transform.position, Quaternion.identity);
+	}
+
+}
